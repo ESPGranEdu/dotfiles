ff05b1af8b4fdd425fb2959c034e7c1650f2bfc6
ESPGranEdu <raplhwigum@gmail.com>
2020-06-25 14:53:26 +0200

    Install Vim-plug

--- install.sh
+++ install.sh
@@ -21,6 +21,8 @@ for files in $(ls -d .[!.]* * | grep -Ev ".git|README.md|install.sh") ; do
 done
 
 # Symlink vim to nvim and install plugins
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 grep -Eq "ubuntu|debian" || ln -svf /usr/bin/nvim /usr/bin/vim
 vim -c "PlugInstall | q | q"
 
