ba2123155c8e31c18de597bf1a56a5d8da78198d
ESPGranEdu <raplhwigum@gmail.com>
2020-06-27 12:15:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,9 @@ done
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 grep -Eq "ubuntu|debian" || ln -svf /usr/bin/nvim /usr/bin/vim
+
 vim -c "PlugInstall | q | q"
+vim -c "CocInstall coc-json coc-python coc-prettier coc-tsserver coc-vimlsp | q | q"
 
 echo -e "\nDotfiles installed !!!"
 echo  "Execute \"source $HOME/.bashrc\" to reload the actual config"
