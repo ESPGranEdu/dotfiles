02c967af76f9db873177f7fcb85d71b33e960b3a
ESPGranEdu <raplhwigum@gmail.com>
2020-06-27 14:06:50 +0200

    Fixed bug install.sh

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ done
 # Symlink vim to nvim and install plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-grep -Eq "ubuntu|debian" || ln -svf /usr/bin/nvim /usr/bin/vim
+grep -Eq "ubuntu|debian" /etc/os-release || ln -svf /usr/bin/nvim /usr/bin/vim
 
 vim -c "PlugInstall | q | q"
 vim -c "CocInstall coc-json coc-python coc-prettier coc-tsserver coc-vimlsp | q | q"
